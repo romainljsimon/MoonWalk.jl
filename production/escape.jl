8b076245b2f6b23742b0b565b193644b65e3fe92
François Villemot <francois.villemot@espci.fr>
2026-04-08 11:48:53 +0200

    feat: same cage for Pareto and escape

--- production/escape.jl
+++ production/escape.jl
@@ -3,9 +3,9 @@ using Random
 
 T = 1000000.0
 amplitude_small = 0.05
-amplitude_large = 0.1
-cage_size = 0.1
-nb_walkers = 100
+amplitude_large = 0.5
+cage_size = 0.2
+nb_walkers = 1000
 
 rates = [300000.0, 100000.0, 30000.0, 10000.0, 3000.0, 1000.0, 300.0, 100.0, 30.0, 10.0]
 
